f58111b64741461620b57fea9f1d1b57c0721c5e
kelkardish <69232304+kelkardish@users.noreply.github.com>
2020-08-31 20:24:09 +0530

    Add alert classes in checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,27 +1,89 @@
 #include <assert.h>
+#include <iostream>
 
-bool is_vital_normal(float value, int upperLimit, int lowerLimit) {    // (normalRange = [ 0 , upperLimit-lowerLimit]
-  return ((unsigned)(upperLimit - value) <= (unsigned)(upperLimit - lowerLimit));
+struct Vitals {
+	//int bedNumber ;
+	const char* vitalName;
+	int measure;
+	int lowerValue;
+	int upperValue;
+};
+
+class AlertInterface {
+public:
+	virtual void raiseAlert(const char* vitalName, const char* level) = 0;
+};
+
+class AlertWtihSMS : public AlertInterface {
+public:
+	void raiseAlert(const char* vitalName, const char* level) override{
+		std::cout << "SMS: " << vitalName << " " << level << std::endl;
+	}
+};
+
+class AlertWtihSound : public AlertInterface {
+public:
+	void raiseAlert(const char* vitalName, const char* level) override{
+		std::cout << "Sound: " << vitalName << " " << level << std::endl;
+	}
+};
+
+//class to test both sound and sms alert
+class AlertWithSoundandSMS : public AlertInterface {
+private:
+	AlertWtihSMS alertSMS;
+	AlertWtihSound alertSound;
+public:
+	void raiseAlert(const char* vitalName, const char* level) {
+		alertSMS.raiseAlert(vitalName, level);
+		alertSound.raiseAlert(vitalName, level);
+	}
+};
+bool vitalIsOk(Vitals* vitalList, int n_Vitals, AlertInterface* alerterPtr ) {
+	
+	for (int i = 0; i < n_Vitals; i++) {
+		if (vitalList[i].measure < vitalList[i].lowerValue) {
+			alerterPtr->raiseAlert(vitalList[i].vitalName, "VITAL IS LOW!");
+			return false;
+		}
+		else if (vitalList[i].measure > vitalList[i].upperValue) {
+			alerterPtr->raiseAlert(vitalList[i].vitalName, "VITAL IS HIGH!");
+			return false;
+		}
+	}
+	return true;
 }
 
 //bpm [70,150], spo2[90,100], respRate[30, 95]
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return ( is_vital_normal(bpm,150,70) && is_vital_normal(spo2,100,90) && is_vital_normal(respRate,95,30) );
-}
-
 int main() {
-  /*
-  //vitalsAreOk(bpm,spo2,respRate)
-  assert(vitalsAreOk(80, 95, 60) == true);        // All Normal
-  assert(vitalsAreOk(60, 95, 60) == false);       //AL_bpm, N_sp02, N_resp
-  assert(vitalsAreOk(180, 95, 60) == false);      //AH_bpm, N_sp02, N_resp
-  assert(vitalsAreOk(80, 95, 22) == false);       //N_bpm,  N_sp02, AL_resp
-  assert(vitalsAreOk(80, 95, 98) == false);       //N_bpm,  N_spo2, AH_resp
-  assert(vitalsAreOk(80, 89, 60) == false);       //N_bpm,  A_sp02, N_resp
-  assert(vitalsAreOk(60, 89, 22) == false);       //All Abnormal Low
-  assert(vitalsAreOk(180, 89, 98) == false);      //All Abnormal High
-  */
-  assert(is_vital_normal(40, 100, 50) == false);
-  assert(is_vital_normal(120, 100, 50) == false);
-  assert(is_vital_normal(92, 100, 50) == true);
+	
+	Vitals vital1[] = { {"bpm",98,70,150},
+					   {"spo2",92,90,100},
+					   {"respRate", 45,30,95} };
+
+	Vitals vital2[] = { {"bpm",65,70,150},			//out of range BPM
+					   {"spo2",92,90,100},
+					   {"respRate", 45,30,95} };
+
+	Vitals vital3[] = { {"bpm",98,70,150},
+					   {"spo2",85,90,100},			//out of range spo2
+					   {"respRate", 45,30,95} };
+
+	Vitals vital4[] = { {"bpm",98,70,150},
+					   {"spo2",92,90,100},
+					   {"respRate", 96,30,95} };	//out of range respRate
+	
+	//int nVitals = sizeof(vital) / sizeof(Vitals);
+
+	//AlertWtihSMS alertSMS;
+	//AlertWtihSound alertSound;
+	
+	AlertWithSoundandSMS awss;
+	assert(vitalIsOk(vital1, 3, &awss) == true);
+	assert(vitalIsOk(vital2, 3, &awss) == false);
+	assert(vitalIsOk(vital3, 3, &awss) == false);
+	assert(vitalIsOk(vital4, 3, &awss) == false);
+
+
+//	assert(vitalIsOk(vital1, nVitals, &alertSMS) == true);
 }
