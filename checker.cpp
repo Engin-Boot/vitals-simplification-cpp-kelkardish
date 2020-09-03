a1e79e0dd7f839d928f116717a6c0ddcfc3392a8
kelkardish <69232304+kelkardish@users.noreply.github.com>
2020-09-03 18:12:15 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 struct Vitals {
-	//int bedNumber ;
+	int bedNumber ;
 	const char* vitalName;
 	int measure;
 	int lowerValue;
@@ -11,20 +11,20 @@ struct Vitals {
 
 class AlertInterface {
 public:
-	virtual void raiseAlert(const char* vitalName, const char* level) = 0;
+	virtual void raiseAlert(int bedNo, const char* vitalName, const char* level) = 0;
 };
 
 class AlertWtihSMS : public AlertInterface {
 public:
-	void raiseAlert(const char* vitalName, const char* level) override{
-		std::cout << "SMS: " << vitalName << " " << level << std::endl;
+	void raiseAlert(int bedNo, const char* vitalName, const char* level) override {
+		std::cout << "SMS:   " << "Bed No: "<<bedNo<< "::"<<vitalName << " " << level << std::endl;
 	}
 };
 
 class AlertWtihSound : public AlertInterface {
 public:
-	void raiseAlert(const char* vitalName, const char* level) override{
-		std::cout << "Sound: " << vitalName << " " << level << std::endl;
+	void raiseAlert(int bedNo, const char* vitalName, const char* level) override {
+		std::cout << "Sound: " << "Bed No: " << bedNo << "::" << vitalName << " " << level << std::endl;
 	}
 };
 
@@ -34,26 +34,26 @@ private:
 	AlertWtihSMS alertSMS;
 	AlertWtihSound alertSound;
 public:
-	void raiseAlert(const char* vitalName, const char* level) {
-		alertSMS.raiseAlert(vitalName, level);
-		alertSound.raiseAlert(vitalName, level);
+	void raiseAlert(int bedNo, const char* vitalName, const char* level) {
+		alertSMS.raiseAlert(bedNo, vitalName, level);
+		alertSound.raiseAlert(bedNo, vitalName, level);
 	}
 };
 
 bool isVitalInRange(Vitals vital, AlertInterface* alerterPtr) {
 	if (vital.measure < vital.lowerValue) {
-			alerterPtr->raiseAlert(vital.vitalName, "VITAL IS LOW!");
-			return false;
+		alerterPtr->raiseAlert(vital.bedNumber, vital.vitalName, "VITAL IS LOW!");
+		return false;
 	}
 	else if (vital.measure > vital.upperValue) {
-			alerterPtr->raiseAlert(vital.vitalName, "VITAL IS HIGH!");
-			return false;
+		alerterPtr->raiseAlert(vital.bedNumber, vital.vitalName, "VITAL IS HIGH!");
+		return false;
 	}
 	return true;
 }
 
-bool vitalsAreOk(Vitals* vitalList, int n_Vitals, AlertInterface* alerterPtr ) {
-	
+bool vitalsAreOk(Vitals* vitalList, int n_Vitals, AlertInterface* alerterPtr) {
+
 	bool flag = true;
 	for (int i = 0; i < n_Vitals; i++) {
 		flag = flag & isVitalInRange(vitalList[i], alerterPtr);
@@ -63,38 +63,34 @@ bool vitalsAreOk(Vitals* vitalList, int n_Vitals, AlertInterface* alerterPtr ) {
 
 //bpm [70,150], spo2[90,100], respRate[30, 95]
 int main() {
-	
-	Vitals vital1[] = { {"bpm",98,70,150},
-					   {"spo2",92,90,100},
-					   {"respRate", 45,30,95} };
 
-	Vitals vital2[] = { {"bpm",65,70,150},			//out of range BPM
-					   {"spo2",92,90,100},
-					   {"respRate", 45,30,95} };
+	Vitals vital1[] = { {13,"bpm",98,70,150},
+			    {13,"spo2",92,90,100},
+			    {13,"respRate", 45,30,95} };
 
-	Vitals vital3[] = { {"bpm",98,70,150},
-					   {"spo2",85,90,100},			//out of range spo2
-					   {"respRate", 45,30,95} };
+	Vitals vital2[] = { {11,"bpm",65,70,150},			//out of range BPM
+			    {11,"spo2",92,90,100},
+			    {11,"respRate", 45,30,95} };
 
-	Vitals vital4[] = { {"bpm",98,70,150},
-					   {"spo2",92,90,100},
-					   {"respRate", 96,30,95} };	//out of range respRate
-	
-	//int nVitals = sizeof(vital) / sizeof(Vitals);
+	Vitals vital3[] = { {7,"bpm",98,70,150},
+			    {5,"spo2",85,90,100},			//out of range spo2
+			    {3,"respRate", 45,30,95} };
 
-	//AlertWtihSMS alertSMS;
-	//AlertWtihSound alertSound;
-	
+	Vitals vital4[] = { {19,"bpm",98,70,150},
+		  	    {19,"spo2",92,90,100},
+			    {19,"respRate", 96,30,95} };		//out of range respRate
+
+
+
 	AlertWithSoundandSMS awss;
 	assert(vitalsAreOk(vital1, 3, &awss) == true);
 	assert(vitalsAreOk(vital2, 3, &awss) == false);
 	assert(vitalsAreOk(vital3, 3, &awss) == false);
 	assert(vitalsAreOk(vital4, 3, &awss) == false);
 
-	assert(isVitalInRange({ "bpm",155,70,150 }, &awss) == false);
-	assert(isVitalInRange({ "spo2",80,90,100 }, &awss) == false);
-	assert(isVitalInRange({ "respRate",85,30,95}, &awss) == true);
+	assert(isVitalInRange({2, "bpm",155,70,150 }, &awss) == false);
+	assert(isVitalInRange({4, "spo2",80,90,100 }, &awss) == false);
+	assert(isVitalInRange({6, "respRate",85,30,95 }, &awss) == true);
 
 
-//	assert(vitalIsOk(vital1, nVitals, &alertSMS) == true);
 }
